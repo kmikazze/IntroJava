754a444f44bbeaf375b4d02d58b01bd59f41f883
kmikazze <camila.mdr@hotmail.com>
2023-05-26 18:43:35 -0300

    Laços de Repetição

--- src/LacosCondicionais/Lista2Exerc1.java
+++ src/LacosCondicionais/Lista2Exerc1.java
@@ -7,6 +7,8 @@ public class Lista2Exerc1 {
 		int quantidade,produto;
 		float preco;
 		
+		System.out.println("-------------------- CARDÁPIO --------------------");
+		System.out.println("\n1. Cachorro quente: R$10,00      2. X-Salada: R$15,00\n3. X-Bacon: R$18,00              4. Bauru: R$12,00\n5. Refrigerante: R$8,00          6. Suco de laranja: R$13,00\n");
 		System.out.println("Digite o número do produto: ");
 		produto = leia.nextInt() ;
 		System.out.println("Digite a quantidade: ");
@@ -16,32 +18,32 @@ public class Lista2Exerc1 {
 		case 1:
 			preco=10.0f;
 			preco=quantidade*preco;
-			System.out.printf(quantidade+" Cachorro quente(s): R$ %.2f",preco);
+			System.out.printf("\n"+quantidade+" Cachorro quente(s): R$ %.2f",preco);
 			break;
 		case 2:
 			preco=15.0f;
 			preco=quantidade*preco;
-			System.out.printf(+quantidade+" X-Salada(s): R$ %.2f",preco);
+			System.out.printf("\n"+quantidade+" X-Salada(s): R$ %.2f",preco);
 			break;
 		case 3:
 			preco=18.0f;
 			preco=quantidade*preco;
-			System.out.printf(+quantidade+" X-Bacon(s): R$ %.2f",preco);
+			System.out.printf("\n"+quantidade+" X-Bacon(s): R$ %.2f",preco);
 			break;
 		case 4:
 			preco=12.0f;
 			preco=quantidade*preco;
-			System.out.printf(+quantidade+" Bauru(s): R$ %.2f",preco);
+			System.out.printf("\n"+quantidade+" Bauru(s): R$ %.2f",preco);
 			break;
 		case 5:
 			preco=8.0f;
 			preco=quantidade*preco;
-			System.out.printf(+quantidade+" Refrigerante(s): R$ %.2f",preco);
+			System.out.printf("\n"+quantidade+" Refrigerante(s): R$ %.2f",preco);
 			break;
 		case 6:
 			preco=13.0f;
 			preco=quantidade*preco;
-			System.out.printf(quantidade+" Suco(s) de laranja: R$ %.2f",preco);
+			System.out.printf("\n"+quantidade+" Suco(s) de laranja: R$ %.2f",preco);
 			break;
 			default:
 		}
